b5a23c115a4ebcf85e89f9c0b8d05c6d1b444cd9
tein1142 <tein1142@hotmail.com>
2021-02-19 20:00:18 +0700

    [ADD] assignment4

--- main.js
+++ main.js
@@ -5,25 +5,28 @@ const app = Vue.createApp({
             lastName: 'Hengnalen',
             role: 'Font-End developer',
             follow: 'Follow',
-            imgProfile: 'https://www.cookierun-kingdom.com/static/f1c6dafc54fd11af37d0f2d744c27054/4797a/pure-vanilla-cookie.png',
+            imgProfile: 'images/profile.jpg',
             pics: [{
-                    image: 'https://yt3.ggpht.com/ytc/AAUvwnjQi01NsCT0G-rbs8hA60aALy89FxZnMvxlIRt1=s900-c-k-c0x00ffffff-no-rj',
-                    title: 'Cookierun1',
-                    id: 'cook1',
+                    image: 'images/Cookie-Run-3.jpg',
+                    title: 'คุกกี้รสนม',
+                    description: 'ท็อปปิ้งแนะนำ : วอลนัตแข็ง / อัลมอนด์(เพิ่มพลังป้องกัน / ลดความเสียหาย)',
+                    description2: 'ท็อปปิ้งทางเลือก : ถั่วลิสงเพื่อสุขภาพ (เพิ่มพลังชีวิต)'  ,
                     done: false,
                     show: false
                 },
                 {
-                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4MmoKUx6H-MY708iSykMwbNVrcqYhuO1B4g&usqp=CAU',
-                    title: 'Cookierun2',
-                    id: 'cook2',
+                    image: 'images/Cookie-Run-1.jpg',
+                    title: 'คุกกี้รสเอสเปรสโซ',
+                    description: 'ท็อปปิ้งแนะนำ : ราสเบอร์รี่ร้อนแรง (เพิ่มพลังโจมตี)',
+                    description2:'ท็อปปิ้งทางเลือก : ช็อกโกแลตชิพรสเข้ม (ลดคูลดาวน์)',
                     done: false,
                     show: false
                 },
                 {
-                    image: 'https://trueid-ugc-prod.imgix.net/partner_files/trueidintrend/201299/%E0%B8%84%E0%B8%B8%E0%B8%81%E0%B8%81%E0%B8%B5%E0%B9%89%E0%B8%88%E0%B8%AD%E0%B8%A1%E0%B9%82%E0%B8%88%E0%B8%A3-%E0%B9%80%E0%B8%AB%E0%B8%A1%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B8%A1%E0%B9%80%E0%B8%9A%E0%B8%B4%E0%B8%A5%E0%B8%8A%E0%B8%B5%E0%B8%AA%20copy.jpg?auto=format&w=1024',
-                    title: 'Cookierun3',
-                    id: 'cook2',
+                    image: 'images/Cookie-Run-2.jpg',
+                    title: 'คุกกี้รสชะเอม',
+                    description: 'ท็อปปิ้งแนะนำ : ช็อกโกแลตชิพรสเข้ม (ลดคูลดาวน์) ',
+                    description2:'ท็อปปิ้งทางเลือก : ราสเบอร์รี่ร้อนแรง (เพิ่มพลังโจมตี)',
                     done: false,
                     show: false
                 }
@@ -35,12 +38,14 @@ const app = Vue.createApp({
         }
 
     },
-    
-    methods: {
 
+    methods: {
+        toggleClear(){
+            this.searchField = ''
+        },
         toggleSearch() {
             this.searchButton = !this.searchButton
-            this.searchField = ''
+            
         },
 
         toggleDone(index) {
@@ -48,15 +53,15 @@ const app = Vue.createApp({
             this.pics[index].done = !this.pics[index].done
         },
 
-        showCanvas(picture){
+        showCanvas(picture) {
             this.canvasPic = picture;
         },
-       
-        closeCanvas(){
+
+        closeCanvas() {
             this.canvasPic = '';
         }
-            
-        
+
+
 
     },
     computed: {
